7d6269e5c477ad0fa4224a0c1adbf5d53dd811e8
171066pwr <171066@student.pwr.edu.pl>
2026-01-21 21:02:25 +0100

    decorated products with random prices

--- src/main/java/com/mycompany/app/model/Product.java
+++ src/main/java/com/mycompany/app/model/Product.java
@@ -1,12 +1,21 @@
 package com.mycompany.app.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.math.BigDecimal;
 
 record Product(
         String productId,
         BigDecimal quantity,
-        Units unit
+        Units unit,
+        @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+        BigDecimal price
 ) {
+
+    Product {
+        price = quantity.multiply(new BigDecimal((productId.hashCode()%100 * productId.hashCode()%100)%100));
+    }
+
     public enum Units {
         UNIT("u"),
         GRAM("g"),
